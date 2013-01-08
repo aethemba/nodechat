e6b25b6e1bc6ff6c1f7e2b01bc239dfe87fd047a
Aksel <aksel.ethem@gmail.com>
2013-01-08 18:16:07 +0100

    beta version working, needs name=null fix

--- app.js
+++ app.js
@@ -34,22 +34,55 @@ app.get('/', routes.index);
 app.get('/users', user.list);
 
 var users = [];
+var msgs = [];
+var max_msgs = 30;
 
 io.sockets.on("connection", function(socket) {
-  var user_id = Math.random();
-  var sid = socket.id;
-  if(users.indexOf(sid) == -1) {
-    users.push({userid:sid});
-  }
-  socket.emit("newuser", {user: user_id});
-  socket.emit("updateusers", users);
+  var randint = Math.floor( (Math.random()*100)+1 );
+  socket.set("nickname", "User " + randint, function(){
+    socket.emit("msgupdated", msgs);
+  });
+
+  socket.on("setnickname", function(name) {
+    socket.set("nickname", name, function() {
+      update_users();
+    });
+  });
 
   socket.on("disconnect", function() {
-    console.log("disconnected user " + sid);
-    var el = users.indexOf(sid);
-    if(el!=-1) users.splice(el, 1);
+    console.log("disconnected user ");
+    io.sockets.emit("updateusers", users);
   });
+
+  update_users();
+
+  socket.on("msg", function(msg) {
+    socket.get("nickname", function(err, name) {
+      console.log("NAME", name);
+      if (max_msgs.length >= 30) {
+        msgs.shift();
+      }
+      msgs.push({name:name, msg:msg})
+      socket.emit("msgupdated", msgs);
+    });
+  });
+
+
 });
 
+function update_users() {
+  users = [];
+  for(var i=0; i < io.sockets.clients().length; i++) {
+    io.sockets.sockets[io.sockets.clients()[i].id].get('nickname', function(err, name) {
+      console.log("NNAME", name);
+      if (name == null) {
+        var randint = Math.floor( (Math.random()*100)+1 );
+        name = "User " + randint;
+      }
+      users.push({id: io.sockets.clients()[i].id, nickname: name});
+    });
+  };
+  io.sockets.emit("updateusers", users);
+}
 
 console.log("Express server listening on port " + app.get('port'));
